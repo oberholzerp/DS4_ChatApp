220f4f908372f5a4cf682c5918949a6ab4861d00
pascalo <pascalo@student.ethz.ch>
2016-11-17 13:01:08 +0100

    initial commit

---
+++ app/src/main/java/ch/ethz/inf/vs/a4/pascalo/chatapp/chatapp/MainActivity.java
@@ -0,0 +1,15 @@
+package ch.ethz.inf.vs.a4.pascalo.chatapp.chatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO all
+        int i = 0;
+    }
+}
